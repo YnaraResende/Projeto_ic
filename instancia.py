7247c175067677bbead8c4cb40f79dd1517ce443
Ynara <55094288+YnaraResende@users.noreply.github.com>
2019-12-15 17:45:58 -0300

    Update instancia.py

--- instancia.py
+++ instancia.py
@@ -2,11 +2,9 @@ class Instancia:
 	
 	def __init__(self,turno,talhoes,equipes,numAtividades):
 		
-		self.numAtividades= numAtividades   #numero de atividades.
-		self.tempoTarefa=[]   #matriz para armazear tempo NxM em que N representa as equipes e M as atividades.
-		self.talhoes=talhoes  #numero de talhoes.
-		self.turno=turno     #turno T que representa o tempo em que as equipes podem trabalhar por dia.
-		self.equipes=equipes #numero de equipes.
-		self.tempoDeslocamento=[] #matriz que armazena tempo de deslocamento entre atividades e entre atividades e origem, entre atividades
-									#de um mesmo talhao nao devem ter tempo calculado
-									#numAtividades+1 X numAtividades+1, 0 representa origem.
+		self.numAtividades= numAtividades   #numero de atividades
+		self.tempoTarefa=[]   			#matriz para armazenar tempo em que cada equipe realiza cada atividade  
+		self.talhoes=talhoes  			#numero de talhoes
+		self.turno=turno     			#turno T que representa o tempo em que as equipes podem trabalhar por dia
+		self.equipes=equipes 			#numero de equipes
+		self.tempoDeslocamento=[] 		#matriz que armazena tempo de deslocamento entre atividades e entre atividades e origem
