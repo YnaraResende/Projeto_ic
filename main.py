25d629b2a06bf2fd6b8a97246a357405eaddef61
Ynara <55094288+YnaraResende@users.noreply.github.com>
2019-12-15 17:47:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ from Instancia import *
 
 def main():
 	
-	arquivo = open("projetoic.txt","r")
+	arquivo = open("BRKGA.txt","r")
 	turno= int(arquivo.readline())
 	talhoes = int(arquivo.readline())
 	equipes = int(arquivo.readline())
 	atividades = int(arquivo.readline())
 	linha = arquivo.readline()
 	objetoInstancia = Instancia(turno,talhoes,equipes,atividades)
-	objetoRota = Rota()
-	resp = objetoRota.CalcularTempo(objetoInstancia,arquivo)
+	objetoRota = Rota(objetoInstancia,arquivo)
+	resp = objetoRota.BRKGA(objetoInstancia)
 	print(resp)
 	arquivo.close()
 	
 if __name__ == '__main__':
 	main()
+
 
