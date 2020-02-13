7aac82b4d7a1ee04d9e5a78da43d701034319937
Ynara <55094288+YnaraResende@users.noreply.github.com>
2020-02-13 20:45:01 -0300

    Update instancia.py

--- instancia.py
+++ instancia.py
@@ -1,10 +1,14 @@
 class Instancia:
 	
-	def __init__(self,turno,talhoes,equipes,numAtividades):
+	def __init__(self,turno,talhoes,equipes,numAtividades,dias):
 		
-		self.numAtividades= numAtividades   #numero de atividades
-		self.tempoTarefa=[]   			#matriz para armazenar tempo em que cada equipe realiza cada atividade  
-		self.talhoes=talhoes  			#numero de talhoes
-		self.turno=turno     			#turno T que representa o tempo em que as equipes podem trabalhar por dia
-		self.equipes=equipes 			#numero de equipes
-		self.tempoDeslocamento=[] 		#matriz que armazena tempo de deslocamento entre atividades e entre atividades e origem
+		self.numAtividades= numAtividades   #numero de atividades  
+		self.talhoes=talhoes  		#numero de talhoes
+		self.turno=turno     		#turno T que representa o tempo em que as equipes podem trabalhar por dia
+		self.equipes=equipes 		#numero de equipes
+		self.dias = dias		#numero de dias 
+		self.tempoDeslocamento=[] 	#matriz que armazena tempo de deslocamento entre regioes e entre regioes e origem
+		self.tempoPadrao= []		#lista que armazena tempo padrão de realizaçao de cada atividade
+		self.tamCliente=[]		#lista que armazena tamanho de cada regiao
+		self.profEquipe=[]		#lista que armazena proficiencia de cada equipe
+		self.custoEquipe=[]		#lista que armazena custo de cada equipe
