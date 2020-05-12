028a7ced766b477e16c33d3b9b7ed1963f4cefad
Ynara <55094288+YnaraResende@users.noreply.github.com>
2020-05-12 10:30:25 -0300

    Alteração na leitura do arquivo

--- Instancia.py
+++ Instancia.py
@@ -3,25 +3,27 @@ class Instancia:
 	def __init__(self):
 		
 		self.atividades={} 			#atividades que devem ser concluidas em cada talhao, em ordem de precedencia
-		self.numAtividades= None  	#numero de atividades  
+		self.ativTalhao= None		#numero de atividades por talhão
+		self.numAtividades= None  	#numero de atividades  no total
 		self.talhoes= None  			#numero de talhoes
-		self.turno= None  				#turno T que representa o tempo em que as equipes podem trabalhar por dia
+		self.turno= None  				#turno que representa o tempo em que as equipes podem trabalhar por dia
 		self.equipes= None				#numero de equipes
 		self.dias=None					#numero de dias 
-		self.tempoDeslocamento=[] 		#matriz que armazena tempo de deslocamento entre regioes e entre regioes e origem
-		self.tempoPadrao= []			#lista que armazena tempo padrão de realizaçao de cada atividade
-		self.tamCliente=[]				#lista que armazena tamanho de cada regiao
-		self.profEquipe=[]				#lista que armazena proficiencia de cada equipe
+		self.tempoDeslocamento=[] 		#matriz que armazena tempo de deslocamento entre regiões e entre regiões e origem
+		self.tempoPadrao= []			#lista que armazena tempo padrão de realização de cada atividade
+		self.tamCliente=[]				#lista que armazena tamanho de cada região
+		self.profEquipe=[]				#lista que armazena proficiência de cada equipe
 		self.custoEquipe=[]				#lista que armazena custo de cada equipe
 		self.tempoTarefa = []			#matriz que armazena o tempo de duração de cada tarefa para cada equipe
 		self.quant = []					#lista que armazena número de grupos de atividades das equipes
+		self.quantMax = None			#número de grupos de atividades no total
 		 	
 	'''
 	Parâmetros:
 		arquivo - arquivo de entrada 
 			
-	Ideia: a função faz a leitura de todo o arquivo de entrada, inicializa o dicionário self.atividades e calcula o tempo de duração de cada atividade
-	 para cada equipe, colocando os valores na matriz self.tempoTarefa.
+	Ideia: a função faz a leitura de todo o arquivo de entrada, inicializa o dicionário self.atividades de acordo com o número de atividades e calcula o
+	 tempo de duração de cada atividade para cada equipe, armazenando os valores na matriz self.tempoTarefa.
 		
 	'''		
 	def inputArq(self, arquivo):
@@ -32,38 +34,29 @@ class Instancia:
 		self.talhoes= int(espaco[1])
 		linhaum = arquivo.readline()
 		espaco = linhaum.split()
-		self.numAtividades = int(espaco[1])
+		self.ativTalhao = int(espaco[1])
+		self.numAtividades = self.ativTalhao*(self.talhoes-1)
 		linhaum = arquivo.readline()
 		espaco = linhaum.split()
 		self.equipes= int(espaco[1])
-		linhaum = arquivo.readline()
-		espaco = linhaum.split()
-		for j in range (1,self.equipes+1):
-			self.quant.append(int(espaco[j]))
 		linhaum = arquivo.readline()
 		espaco = linhaum.split()
 		self.turno= int(espaco[1])
 		linhaum = arquivo.readline()
 		espaco = linhaum.split()
 		self.dias= int(espaco[1])
-		
 		arquivo.readline()
 
-		
 		for i in range (0, (self.talhoes-1)):	#self.talhoes-1 porque self.talhoes representa numero de talhoes + deposito
 			self.atividades[i] = []
 			
 		for i in range (0,(self.talhoes-1)):
-			linha = arquivo.readline()
-			espaco = linha.split()
-			cont = int(espaco[1])
-			ite = 2
-			while cont != -1:
-				self.atividades[i].append(cont)
-				cont = int(espaco[ite])
-				ite = ite+1
-				
-		arquivo.readline()
+			if i != 0:
+				cont = (self.atividades[i-1][self.ativTalhao-1])+1
+			else:
+				cont = i+1
+			for z in range(cont, cont+self.ativTalhao):
+				self.atividades[i].append(z)
 		
 		for i in range (0, self.talhoes):
 			linha = arquivo.readline()
@@ -75,11 +68,10 @@ class Instancia:
 			self.tempoDeslocamento.append(tmp[:])
 			
 		arquivo.readline()
-		
 		linha = arquivo.readline()
 		espaco =  linha.split()
 		
-		for i in range (0, self.numAtividades):
+		for i in range (0, self.ativTalhao):
 			elemento=float(espaco[i])
 			self.tempoPadrao.append(elemento)
 			
@@ -105,17 +97,23 @@ class Instancia:
 			elemento=float(espaco[i])
 			self.custoEquipe.append(elemento)
 		
-		#inicialização self.tempoTarefa
+		linhaum = arquivo.readline()
+		espaco = linhaum.split()
+		for j in range (1,self.equipes+1):
+			self.quant.append(int(espaco[j]))
+			
+		linhaum = arquivo.readline()
+		espaco = linhaum.split()
+		self.quantMax= int(espaco[1])
+		
 		for i in range (0, self.numAtividades):
 			linha = []
 			for j in range (0, self.equipes):
 				linha.append(" ")
 			self.tempoTarefa.append(linha[:])
 		
-		#cálculo da duração das atividades por cada equipe e armazenamento dos valores na matriz self.tempoTarefa
 		for c,v in self.atividades.items():
 			for ajuda in range (0, len(v)):
 				i =  self.atividades[c][ajuda]
 				for j in range (0, self.equipes):
-					self.tempoTarefa[i-1][j]= (self.tamCliente[c+1]*self.tempoPadrao[i-1])/self.profEquipe[j]
-			
+					self.tempoTarefa[i-1][j]= (self.tamCliente[c+1]*self.tempoPadrao[ajuda])/self.profEquipe[j]
